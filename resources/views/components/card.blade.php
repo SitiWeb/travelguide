9e05d4d884b04b1a5ac3315e3f2c7813bf0b5ba4
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-04 11:59:32 +0200

    added address to cards

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,6 +2,9 @@
 <div class="p-8 ">
     <div class="p-4 rounded-2xl shadow-2xl" style="background-color:#004578; color:white;">
         <div class="graphik-font text-xl mb-2">{{$index}}. {{$item->title}}</div>
+        @isset($item->address)
+        <div class="mb-2"><div class="graphik-font text-color-camel-alt text-sm" >{{$item->address}}</div></div>
+        @endisset
         @isset($item->url)
         <div class="mb-2"><a class="graphik-font text-color-camel-alt text-sm" href="{{$item->url}}">{{$item->url}}</a></div>
         @endisset
