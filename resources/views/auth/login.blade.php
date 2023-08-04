239e3bd784f5c9bbdba30a4cfa486a2bf0b6176a
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-04 09:40:12 +0200

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,19 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="card-container relative mx-auto max-w-xs h-96">
+    <!-- Card 1 - Light Blue -->
+    <div id="card-1" class="overflow-hidden background-blue-camel-alt  absolute p-6 rounded-lg  card h-80" style="transform: translateX(-55%) translateY(-45%) rotate(-10deg);width:85%;min-width:250px;">
+  
+    </div>
+    
+    <!-- Card 2 - Yellow -->
+    <div id="card-2" class="overflow-hidden background-blue-camel-yellow absolute p-6 rounded-lg w-full card h-80" style="transform: translateX(-45%) translateY(-50%) rotate(10deg);width:75%;min-width:250px;">
 
+    </div>
+    
+    <!-- Card 3 - White -->
+    <div id="card-3" class="overflow-hidden w-full bg-white absolute p-6 rounded-lg w-full card h-80" style="transform: translateX(-50%) translateY(-50%);min-width:300px; ">
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -44,4 +56,26 @@
             </x-primary-button>
         </div>
     </form>
+    </div>
+  </div>
+
+  <style>
+    .card-container {
+      perspective: 1000px; /* Create 3D perspective */
+    }
+    
+    .card {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform-style: preserve-3d; /* Preserve 3D transformations */
+      transition: transform 0.3s ease; /* Add smooth transition effect on hover */
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add box shadow to each card */
+    }
+    
+    .card:hover {
+      transform: translateY(-50%) rotateX(10deg); /* On hover, shift and tilt the cards */
+    }
+  </style>
+   
 </x-guest-layout>
