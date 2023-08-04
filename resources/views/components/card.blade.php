9244cd1b55cc5b82011c1e35f54e44a0912e364d
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-04 15:23:46 +0200

    Changesd website icon

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,11 +6,21 @@
         <div class="mb-2"><div class="graphik-font text-color-camel-alt text-sm" >{{$item->address}}</div></div>
         @endisset
         @isset($item->url)
-        <div class="mb-2"><a class="graphik-font text-color-camel-alt text-sm" href="{{$item->url}}">{{$item->url}}</a></div>
+        <div class="mb-2"><a class="graphik-font text-color-camel-alt " href="{{ $item->url }}">
+                    <div class="flex items-center gap-1">
+                        <svg fill="#00abe3" xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                        </svg>
+                        <div>Visit website</div>
+                    </div>
+                </a>
+            </div>
         @endisset
-        <div class="graphik-font text-xs">{{$item->description}}</div>
+        <div class="graphik-font text-sm">{{$item->description}}</div>
         @isset($item->price)
-        <div class="text-right graphik-font text-xs mt-2">{{$item->price}}</div>
+        <div class="text-right graphik-font text-sm mt-2">{{$item->price}}</div>
         @endisset
     </div>
 </div>
