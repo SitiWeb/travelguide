a14f57c1574b42519db9391bb721f0bb2228bf3c
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-07 14:35:02 +0200

    Background- slide to discover

--- resources/views/components/header-destination.blade.php
+++ resources/views/components/header-destination.blade.php
@@ -2,7 +2,7 @@
     <div class='h-64'>
         <div class="bg-cover bg-center w-full flex flex-column items-center h-64 z-10 fixed max-w-7xl"
             style="background-image: url('{{ asset('storage/' . $destination->image_path) }}');flex-direction:column;">
-            <div class="text-white mt-2 camel-font">Slide to Discover</div>
+            <div class="text-white mt-2 camel-font p-2 rounded" style="background-color:#004578 ;">Slide to Discover</div>
         </div>
     </div>
 @endif
