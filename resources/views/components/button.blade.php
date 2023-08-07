785013d5a5cbc097ddcf9b273295257d6230255b
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-07 13:25:01 +0200

    id is not required

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,8 +1,6 @@
 @props(['url','id'])
 
-@if(!isset($id))
-$id = '';
-@endif
-<a href="{{$url}}" class="camel-font text-white background-blue-camel-alt w-full text-center py-2 px-6 rounded-full block" id="{{$id}}">
+
+<a href="{{$url}}" class="camel-font text-white background-blue-camel-alt w-full text-center py-2 px-6 rounded-full block" id="@if(isset($id)) echo $id; @endif">
     {{$slot}}
 </a>
