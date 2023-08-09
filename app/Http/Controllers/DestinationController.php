83835ae3f42317af5e9e22b5bd39dd80cab4f13b
roberto Guagliardo <administratie@sitiweb.nl>
2023-08-09 15:31:02 +0200

    Fix for linux

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -128,7 +128,7 @@ class DestinationController extends Controller
         }
         // Handle image upload if a new image file is provided
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
-            $imagePath = $request->file('image')->store('images', 'public'); // Change the storage path as per your requirement
+            $imagePath = $request->file('image')->store('images', 'publicv'); // Change the storage path as per your requirement
             // Save the image path to the destination
             $destination->image_path = $imagePath;
             $destination->save();
